3ead9fbd4f05e1ea018512d9d5a263c95270574d
William Yau <122402171+WorldAdrastea@users.noreply.github.com>
2023-11-04 22:31:49 +0000

    lambda init
    lambda init

---
+++ index.js
@@ -0,0 +1,42 @@
+const { S3Client, GetObjectCommand, PutObjectCommand } = require("@aws-sdk/client-s3");
+const sharp = require('sharp');
+
+const s3Client = new S3Client({
+    region: "us-east-1",
+    endpoint: "http://locahost:4566", 
+    forcePathStyle: true
+});
+
+exports.handler = async (event, context) => {
+    const srcBucket = event.Records[0].s3.bucket.name;
+    const srcKey = decodeURIComponent(
+        event.Records[0].s3.object.key.replace(/\+/g, ' ')
+    );
+    const dstBucket = srcBucket;
+    const dstKey = `resized/${srcKey}`;
+
+    try {
+        // Fetch the original image from S3
+        const getObjectParams = { 
+            Bucket: srcBucket, 
+            Key: srcKey,
+        };
+        const imageData = await s3Client.send(new GetObjectCommand(getObjectParams));
+
+        // Resize the image using Sharp.
+        const resizedImage = await sharp(imageData.Body).resize(200, 200).toBuffer();
+
+        // Upload the resized image back to S3
+        const putObjectParams = {
+            Bucket: dstBucket,
+            Key: dstKey,
+            Body: resizedImage,
+        };
+        await s3Client.send(new PutObjectCommand(putObjectParams));
+
+        return 'Image resized and saved';
+    }   catch (err) {
+            console.error('Error:', err);
+        throw err;
+    }
+};
